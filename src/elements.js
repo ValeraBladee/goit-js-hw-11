b5020b5c76e28849eea482f4f121c7c004f52f77
ValeraBladee <potniyschenok@gmail.com>
2023-09-07 14:46:25 +0300

    UPDATE

---
+++ src/elements.js
@@ -0,0 +1,7 @@
+export const elements = {
+    galleryEl: document.querySelector('.gallery'),
+    searchInput: document.querySelector('input[name="searchQuery"'),
+    searchForm: document.getElementById('search-form'),
+    loaderEl: document.querySelector('.loader'),
+  };
+  
